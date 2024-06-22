818875090e5b4f55fd5cdccb3a8a7c98c5297a54
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-06-22 17:35:56 -0300

    feat: add base App.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export const Header = (props) => {
+    console.log(props);
+    return (
+        <>
+            <header>
+                <h1>My React App</h1>
+            </header>
+        </>
+    );
+};
