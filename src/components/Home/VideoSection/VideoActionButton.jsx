88d98434956e27bbbe4865f99b077c3e6a73a3ab
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-07-11 17:00:00 -0300

    feat: add video action button on video cards

--- src/components/Home/VideoSection/VideoActionButton.jsx
+++ src/components/Home/VideoSection/VideoActionButton.jsx
@@ -1,28 +1,42 @@
-import { PropTypes } from 'prop-types';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const StyledButton = styled.button`
     background-color: transparent;
     border: none;
     color: var(--text-color);
     display: flex;
-    gap: 20px;
+    gap: 10px;
     cursor: pointer;
     align-items: center;
+
+    img {
+        width: 20px;
+        height: 20px;
+    }
+
+    span {
+        font-size: 1.3rem;
+    }
+    &:hover {
+        color: var(--text-color-blue);
+    }
 `;
 
-const VideoSectionButton = ({ img, text }) => {
+const VideoSectionButton = ({ icon, text, onClick }) => {
+
     return (
-        <StyledButton>
-            <img src={img} alt={text} />
+        <StyledButton onClick={onClick}>
+            <img src={icon} alt={text} />
             <span>{text}</span>
         </StyledButton>
     );
-}
+};
 
 VideoSectionButton.propTypes = {
-    img: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    onClick: PropTypes.func.isRequired,
 };
 
 export default VideoSectionButton;
