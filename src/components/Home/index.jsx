ddd8a84488fc6af70f8bc54d9e949f07156cfa60
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-07-12 19:00:00 -0300

    refactor: rework on video category to prevent reload on update and delete

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,27 +1,57 @@
+import { useState, useEffect } from 'react';
 import Header from "../Header";
 import Footer from "../Footer";
-import styled from 'styled-components'
-import Categoria from "./VideoSection/VideoCategory";
+import styled from 'styled-components';
+import VideoCategory from "./VideoSection/VideoCategory";
 import Banner from "./BannerSection/Banner";
+import { useVideo } from '../../hooks/useVideo';
 
 const ContainerCategorias = styled.div`
-    padding: 8rem;
-    display: flex;
+    padding: 8rem 4rem;
     flex-direction: column;
     gap: 10rem;
     align-items: center;
     margin-bottom: 5rem;
-`
+    background: #000509;
+`;
 
 const Home = () => {
+    const [categorias, setCategorias] = useState([]);
+    const [videos, setVideos] = useState([]);
+    const { deleteVideo } = useVideo();
+
+    useEffect(() => {
+        fetch('http://localhost:3002/videos')
+            .then(response => response.json())
+            .then(data => {
+                const categoryNames = new Set(data.map(video => video.categoria));
+                setCategorias([...categoryNames]);
+                setVideos(data);
+            })
+            .catch(error => {
+                console.error('Erro ao buscar dados:', error);
+            });
+    }, []);
+
     return (
         <>
             <Header />
             <Banner />
             <ContainerCategorias>
-                <Categoria titulo="Front-End" color="#6BD1FF" />
-                <Categoria titulo="Back-End" color="#00C86F" />
-                <Categoria titulo="Mobile" color="#FFBA05" />
+                {categorias.map(categoria => (
+                    <VideoCategory
+                        key={categoria}
+                        titulo={categoria}
+                        color={
+                            categoria === "front-end" ? "#6BD1FF" :
+                            categoria === "back-end" ? "#00C86F" :
+                            categoria === "mobile" ? "#FFBA05" : "#FFFFFF"
+                        }
+                        videos={videos.filter(video => video.categoria === categoria)}
+                        onDelete={deleteVideo}
+                         setVideos={setVideos}
+                    />
+                ))}
             </ContainerCategorias>
             <Footer />
         </>
