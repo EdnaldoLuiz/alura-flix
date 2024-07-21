d27851a05ccf83364089f51ac443322548d49d5e
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-07-21 09:00:00 -0300

    feat: update main app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import GlobalStyles from "../public/styles/GlobalStyles";
+import AugmentedUI from "../public/styles/AugmentedUI";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
     return (
         <main>
+            <AugmentedUI />
             <GlobalStyles />
             <Header />
             <Outlet />
